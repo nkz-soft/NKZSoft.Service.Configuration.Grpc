6235fdf28bfe7065bbccd76ba7929fb2d7785a32
nkz <5499916+nkz-soft@users.noreply.github.com>
2024-12-19 20:11:20 +0300

    Support for .NET 9 (#78)
    * Support for .NET 9

--- tests/GrpcTestServerTests/GlobalUsings.cs
+++ tests/GrpcTestServerTests/GlobalUsings.cs
@@ -6,6 +6,5 @@ global using Grpc.Core.Interceptors;
 global using Grpc.Net.Client;
 global using GrpcTestServer.Services;
 global using Microsoft.AspNetCore.Hosting;
-global using Microsoft.AspNetCore.Mvc.Testing;
 global using ProtoBuf.Grpc.Client;
 global using Xunit;
